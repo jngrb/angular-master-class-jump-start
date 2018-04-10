8f12b56b2142ebd03b0dfc0b75d17fd02c34f500
Jan Grieb <Jan.Grieb@rohde-schwarz.com>
2018-04-10 14:43:05 +0200

    Task 4: mocking http requests

--- src/app/contacts.service.spec.ts
+++ src/app/contacts.service.spec.ts
@@ -1,8 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, inject, async } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-import { delay } from 'rxjs/operators';
+import { delay, catchError } from 'rxjs/operators';
 
 import { API_ENDPOINT } from './app.tokens';
 
@@ -13,52 +14,80 @@ import { ContactsService } from './contacts.service';
 
 describe('ContactsService', () => {
 
+  const MOCKED_API_ENDPOINT = 'http://localhost:4201/api';
+  const mockResponse: Contact[] = [
+    CONTACT_DATA[2], CONTACT_DATA[4], CONTACT_DATA[6]
+  ];
+
   let service: ContactsService;
+  let backend: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [],
       imports: [
-        HttpClientModule
+        HttpClientTestingModule
       ],
       providers: [
         ContactsService,
-        { provide: API_ENDPOINT, useValue: 'http://localhost:4201/api' }
+        { provide: API_ENDPOINT, useValue: MOCKED_API_ENDPOINT }
       ]
     });
 
-    // alternative DI resolution: service = TestBed.get(ContactsService);
-    /* get the dependency resolution from the scope of a component:
-     * fixture.debugElement.injector.get(DependencyToken) */
+    service = TestBed.get(ContactsService);
+    backend = TestBed.get(HttpTestingController);
   });
 
-  beforeEach(inject([ContactsService], contactsService => {
-    service = contactsService;
-  }));
-
-  it('should provide all contacts - sync test', () => {
-    service.getContacts().pipe( delay( 100 ) )
-      .subscribe( results => {
-        expect(results.length).toBe(0); // will not fail because the delay prevent evaluation
-        expect(results.length).toBeGreaterThan(0);
-      });
-
-    const TEST_ID = '2';
-    service.getContact(TEST_ID).subscribe( result => {
-      expect(result.id).toMatch(TEST_ID); // cannot be toBe, because of type mismatch
-    });
-  });
-
-  it('should provide all contacts', async(() => {
+  it('should provide all contacts', () => {
     service.getContacts().subscribe( results => {
-      expect(results.length).toBeGreaterThan(0);
+      expect(results.length).toBe(mockResponse.length);
     });
-  }));
+
+    const req = backend.expectOne(MOCKED_API_ENDPOINT + '/contacts');
+    backend.expectNone(MOCKED_API_ENDPOINT + '/contact');
+
+    expect(req.cancelled).toBeFalsy();
+    req.flush({items: mockResponse});
+
+    backend.verify();
+  });
 
   it('should provide the contacts with ID=2', async(() => {
     const TEST_ID = '2';
     service.getContact(TEST_ID).subscribe( result => {
       expect(result.id).toMatch(TEST_ID); // cannot be toBe, because of type mismatch
     });
+
+    backend.expectNone(MOCKED_API_ENDPOINT + '/contacts');
+    backend.expectOne(MOCKED_API_ENDPOINT + `/contacts/${TEST_ID}`).flush({item: CONTACT_DATA[2]});
+    backend.verify();
+  }));
+
+  it('should be able to update the contact with ID=2', async(() => {
+    const updatedContact = {...CONTACT_DATA[2], name: 'Ali Baba'};
+    service.updateContact(updatedContact).subscribe( result => {
+      expect(result).toEqual(updatedContact);
+    });
+
+    backend.expectNone(MOCKED_API_ENDPOINT + '/contacts');
+    const req = backend.expectOne(MOCKED_API_ENDPOINT + `/contacts/${updatedContact.id}`);
+    expect(req.request.method).toBe('PUT');
+    req.flush({item: updatedContact}, {status: 200, statusText: 'OK'});
+    backend.verify();
+  }));
+
+  it('should not be able to update the contact with an invalid ID', async(() => {
+    const updatedContact = {...CONTACT_DATA[2], name: 'Ali Baba', id: 100};
+    service.updateContact(updatedContact)
+      .pipe( catchError( (err: HttpErrorResponse) => { expect(err).toBeDefined(); return []; } ) )
+      .subscribe( result => {
+        expect(false).toBeTruthy();
+      });
+
+    backend.expectNone(MOCKED_API_ENDPOINT + '/contacts');
+    const req = backend.expectOne(MOCKED_API_ENDPOINT + `/contacts/${updatedContact.id}`);
+    expect(req.request.method).toBe('PUT');
+    req.flush({item: updatedContact}, {status: 404, statusText: 'Contact not found'});
+    backend.verify();
   }));
 });
